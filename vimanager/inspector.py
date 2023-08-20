b944e3cb1d29cc4deab46a6f000f5cc1e870985f
Lee-matod <lee.underscore.matod@gmail.com>
2023-08-20 16:12:02 -0400

    Highlight liked songs in red

--- vimanager/inspector.py
+++ vimanager/inspector.py
@@ -37,9 +37,11 @@ from .utils import find_playlist, get_connection
 @click.argument("playlist_name", required=False)
 def inspect(playlist_db: click.File, playlist_name: Optional[str]):
     """Inspect a ViMusic playlist from its backup database.
-    
+
     If no playlist names are given, then it will open the database and output
     all playlists found, prompting a selection.
+
+    Liked songs are highlighted in red.
     """
     conn = get_connection(playlist_db.name)
     cursor = conn.cursor()
@@ -49,28 +51,36 @@ def inspect(playlist_db: click.File, playlist_name: Optional[str]):
         if not tracks:
             click.echo(f"{Back.RED}Playlist is empty.{Back.RESET}")
             return
+
+        # Padding
         max_artist_name = len(max([t.artist for t in tracks], key=len))
         max_song_name = len(max([t.title for t in tracks], key=len))
         index_padding = len(str(len(tracks)))
 
-        fmt = (
-            f"  {Fore.YELLOW}{{0:{index_padding}}}. "
-            f"{Fore.WHITE}{{1.title:{max_song_name}}} "
-            f"{Fore.GREEN}{{1.artist:{max_artist_name}}}{Fore.RESET} "
-            f"{Fore.RED}{{1.id}}"
-        )
+        # Playlist info
         click.echo(f"\n{Style.BRIGHT}-*- {Fore.MAGENTA}#{playlist.id} {Fore.BLUE}{playlist.name}{Fore.RESET} -*-")
         click.echo(
             f"Found {Style.BRIGHT}{Fore.RED}{len(tracks)}{Style.RESET_ALL} total tracks in playlist; "
             f"{Style.BRIGHT}{Fore.RED}{len(set([t.artist.lower() for t in tracks]))}{Style.RESET_ALL} unique artists.\n"
         )
+
+        # Track column headers
         click.echo(
             f"  {' ':{index_padding}}  "
             f"{Style.BRIGHT}{Fore.CYAN}{Back.BLACK}{'Song':^{max_song_name}}{Back.RESET} "
             f"{Back.BLACK}{'Artist':^{max_artist_name}}{Back.RESET} "
             f"{Back.BLACK}{'Song ID':^11}{Style.RESET_ALL}"
         )
-        click.echo("\n".join(fmt.format(idx, t) for idx, t in enumerate(tracks, start=1)))
+
+        # Track info
+        for idx, track in enumerate(tracks, start=1):
+            click.echo(
+                f"  {Fore.YELLOW}{idx:{index_padding}}. "
+                + (f"{Style.BRIGHT}{Fore.RED}" if track.liked else f"{Fore.WHITE}")
+                + f"{track.title:{max_song_name}}{Style.RESET_ALL} "
+                + f"{Fore.GREEN}{track.artist:{max_artist_name}} "
+                + f"{Fore.BLUE}{track.id}"
+            )
     finally:
         cursor.close()
         conn.close()
